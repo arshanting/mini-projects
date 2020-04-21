5b26f4bd4b533cbc97400bc11a421edc146de35a
arshanting <arshanting@gmail.com>
2020-04-21 22:50:45 +0800

    removed unnecessary codes and adjusted some functions

--- to-do-list/javascript/script.js
+++ to-do-list/javascript/script.js
@@ -1,91 +1,65 @@
-(function() {
-  // Selectors
-  const inputTask = document.getElementById('inputTask');
-  const formAddTask = document.getElementById('addTask');
-  const taskHolder = document.getElementById('taskList');
-  const doneHolder = document.getElementById('doneList');
+// Selectors
+const inputTask = document.getElementById('inputTask');
+const formAddTask = document.getElementById('addTask');
+const taskHolder = document.getElementById('taskList');
+const doneHolder = document.getElementById('doneList');
 
-  // Functions
-  document.addEventListener('DOMContentLoaded', function() {
-    // init functions
-    updateTaskItems();
-    addTask();
-  });
-  let taskItems = [];
+let taskItems = [];
 
-  addTask = () => {
+addTask = () => {
+  formAddTask.addEventListener('submit', function(e) {
+    const taskVal = inputTask.value;
+    
+    e.preventDefault(); // prevent form submission
 
-    formAddTask.addEventListener('submit', function(e) {
-      const taskVal = inputTask.value;
-      
-      e.preventDefault(); // prevent form submission
-
-      if (taskVal == null || taskVal == undefined || taskVal <= taskVal.length) { // if #inputTask is empty
-        alert('Please enter your task');
-      } else { // if taskItems/localStorage has value
-        taskItems.push({'id': Date.now(), 'task': taskVal, 'status': 'pending'});
-        taskHolder.innerHTML += toDoTemplate(taskItems);
-      }
-      
-      localStorage.setItem('tasks', JSON.stringify(taskItems));
-      
-      console.log('taskItems', taskItems);
-      console.log(taskVal);
-      
-      inputTask.value = '';
-    });
-  }
-
-  toDoTemplate = (object) => {
-    // // Create LI element
-    // const todoLi = document.createElement('li');
-    // todoLi.id = object.id;
-    // todoLi.innerText = object.task;
-
-    // // Create LABEL element
-    // const label = document.createElement('label');
-    // todoLi.appendChild(label);
-
-    // // Create CHECKBOX element
-    // const checkbox = document.createElement('input');
-    // checkbox.type = 'checkbox';
-    // label.appendChild(checkbox);
-
-    // // Create TRASH button
-    // const btnDelete = document.createElement('button');
-    // btnDelete.innerText = 'Delete';
-    // btnDelete.type = 'button';
-    // btnDelete.classList.add('btn-delete');
-    // todoLi.appendChild(btnDelete);
-
-    // // APPEND THE CREATED LI ELEMENT
-    // taskHolder.appendChild(todoLi);
-
-    return `<li id="${object.id}" data-status="${object.status}">
-              <label><input type="checkbox"></label>${object.task}
-              <button type="button" class="btn-delete">Delete</button>
-            </li>`;
-  }
-
-  checkLocalStorage = (object) => {
-    let taskItems;
-
-    if (localStorage.getItem('tasks') === null) { // if localStorage is empty, set the taskItems into blank array
-      taskItems = [];
-    } else { // if localStorage has value, set the taskItems into it.
-      taskItems = JSON.parse(localStorage.getItem('tasks'));
-    }
-  }
-
-  updateTaskItems = (object) => {
-    if (localStorage.getItem('tasks') === null) { // if localStorage is empty, set the taskItems into blank array
-      taskItems = [];
-    } else { // if localStorage has value, set the taskItems into it.
-      taskItems = JSON.parse(localStorage.getItem('tasks'));
+    if (taskVal == null || taskVal == undefined || taskVal <= taskVal.length) { // if #inputTask is empty
+      alert('Please enter your task');
+    } else { // if taskItems/localStorage has value
+      taskItems.push({'id': taskItems.length + 10, 'task': taskVal, 'status': false});
+      // taskHolder.innerHTML += toDoTemplate(taskItems);
     }
     
-    taskItems.forEach(task => { // display all the taskItems from the 'tasks' array (localStorage)
-      taskHolder.innerHTML += toDoTemplate(task);
-    });
+    localStorage.setItem('tasks', JSON.stringify(taskItems));
+    
+    console.log('taskItems', taskItems);
+    console.log(taskVal);
+    
+    inputTask.value = '';
+  });
+}
+
+toDoTemplate = (object) => {
+  return `<li id="${object.id}" data-status="${object.status}">
+            <label><input type="checkbox"></label>${object.task}
+            <button type="button" class="btn-delete">Delete</button>
+          </li>`;
+}
+
+checkLocalStorage = () => {
+  if (localStorage.getItem('tasks') === null) { // if localStorage is empty, set the taskItems into blank array
+    taskItems = [];
+  } else { // if localStorage has value, set the taskItems into it.
+    taskItems = JSON.parse(localStorage.getItem('tasks'));
   }
-})();
+}
+
+updateTaskItems = (object) => {
+  if (localStorage.getItem('tasks') === null) { // if localStorage is empty, set the taskItems into blank array
+    taskItems = [];
+  } else { // if localStorage has value, set the taskItems into it.
+    taskItems = JSON.parse(localStorage.getItem('tasks'));
+  }
+  
+  taskItems.forEach(task => { // display all the taskItems from the 'tasks' array (localStorage)
+    taskHolder.innerHTML += toDoTemplate(task);
+    // console.log(task.id);
+  });
+}
+
+// init functions
+document.addEventListener('DOMContentLoaded', function() {
+  checkLocalStorage();
+  updateTaskItems();
+  addTask();
+  // console.log(taskItems);
+});
